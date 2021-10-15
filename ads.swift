dfa68e387569470dfe816acfa63f2aa7cb063ba3
BlurryTCP <91014329+BlurryTCP@users.noreply.github.com>
2021-10-15 14:43:58 +0200

    Update ads.swift

--- ads.swift
+++ ads.swift
@@ -1,1 +1,10 @@
-
+var main_sender = [String]()
+func sender(pickup: String) -> String {
+    let tc = pickup + " packet"
+    return tc
+}
+main_sender.append("8918029932")
+print(main_sender)
+let old = Int.random(in: 0...8918029932)
+print("",sender(pickup:"send "),old)
+// making full code later im busy
